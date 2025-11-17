ee8d2081900497dd46eed014b3d1d20c891c3f75
Tasbeeh <tasbeh.takrore@gmail.com>
2025-11-17 21:42:56 +0200

    Update test_coloring file

--- tests/test_coloring.py
+++ tests/test_coloring.py
@@ -1,21 +1,45 @@
+import  random
+
 from models.coloring_state import Coloring
 from models.graph import Graph
 
-
-def test_conflicts():
+def create_triangle_graph() -> Graph:
     g = Graph()
-    for i in range(3):
+    for _ in range(3):
         g.add_vertex()
 
     g.add_edge(0, 1)
     g.add_edge(1, 2)
     g.add_edge(0, 2)
+    return g
 
-    c = Coloring(g, num_colors=2)
 
-    c.set_color(0, 0)
-    c.set_color(1, 0)
-    c.set_color(2, 1)
+def test_conflicts_on_triangle():
+    graph = create_triangle_graph()
+    coloring = Coloring(graph, num_colors=3)
 
-    assert c._num_conflicts == 1
+    coloring.set_color(0, 0)
+    coloring.set_color(1, 0)
+    coloring.set_color(2, 0)
 
+    assert coloring.num_conflicts == 3
+
+    coloring.set_color(0, 0)
+    coloring.set_color(1, 1)
+    coloring.set_color(2, 2)
+
+    assert coloring.num_conflicts == 0
+
+
+def test_modify_one_vertex_changes_at_least_one_color():
+    random.seed(0)
+
+    graph = create_triangle_graph()
+    coloring = Coloring(graph, num_colors=3)
+    coloring.randomize()
+
+    before = coloring.get_colors()
+    coloring.modify_one_vertex()
+    after = coloring.get_colors()
+
+    assert before != after
